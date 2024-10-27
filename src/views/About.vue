60380ff03964ea99ef495663d7c96763521136fe
lordofthescrolls <hi@lordofthescrolls.com>
2024-10-27 13:26:52 +1100

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,11 @@
 <template>
 <div class="container">
-  <h1 class="text-center">About Scrolly Domains</h1>
+  <h1 class="text-center">About Lord of the Scrolls Domains</h1>
   
   <div class="row mt-3">
     <div class="col-md-8 offset-md-2">
       <p>
-        Scrolls Domains is a Web3 Social username service where anybody can mint their very own web3 domain username with a .scrolls extension.
+        Lord of the Scrolls Domains is a Web3 Social username service where anybody can mint their very own web3 domain username with a .scrolls extension.
       </p>
 
     </div>
